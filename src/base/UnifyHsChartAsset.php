ec9e7369ff17aca730cb7891d577f214933864fb
Nelya-home <nelya@rusoft.ru>
2019-08-20 13:19:11 +0300

    Новый Asset для counters в виде спидометра

---
+++ src/base/UnifyHsChartAsset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\assets\unify\base;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class UnifyHsChartAsset extends UnifyAsset
+{
+
+    public $js = [
+        'assets/vendor/circles/circles.min.js',
+        'assets/js/components/hs.chart-pie.js'
+    ];
+
+    public $depends = [
+        UnifyHsCoreAsset::class
+    ];
+}
