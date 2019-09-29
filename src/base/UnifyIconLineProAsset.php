a24fb7dff6adc28ee54a2f6944c4e7f2ae72733d
semenov <semenov@skeeks.com>
2019-09-29 22:39:25 +0300

    dev

---
+++ src/base/UnifyIconLineProAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\assets\unify\base;
+
+use yii\web\YiiAsset;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class UnifyIconLineProAsset extends UnifyAsset
+{
+    public $css = [
+        'assets/vendor/icon-line-pro/style.css',
+    ];
+
+    public $js = [
+    ];
+
+    public $depends = [
+    ];
+}
