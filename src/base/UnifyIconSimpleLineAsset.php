63ea3b50294482263d0ad420791d0e2f8d545978
Semenov <semenov@skeeks.com>
2020-01-29 11:33:31 +0300

    dev

---
+++ src/base/UnifyIconSimpleLineAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\assets\unify\base;
+
+use yii\web\YiiAsset;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class UnifyIconSimpleLineAsset extends UnifyAsset
+{
+    public $css = [
+        'assets/vendor/icon-line/css/simple-line-icons.css',
+    ];
+
+    public $js = [
+    ];
+
+    public $depends = [
+    ];
+}
